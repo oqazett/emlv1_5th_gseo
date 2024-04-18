089020b65f0a1fdf97477f645068b1d9a3e37de8
oqazett <oqazett@gmail.com>
2024-04-18 17:03:31 +0900

    feat:[GwangSeon Eo] led_entity_init의 pin io mode 네임 변경

--- 5th/gseo/ddd_pwm/domain/led/entity/led.c
+++ 5th/gseo/ddd_pwm/domain/led/entity/led.c
@@ -1,10 +1,10 @@
 #include "led.h"
 #include "../../pin/pin_map.h"
 
-void led_entity_init (PIN_MAP pin_map, PIN_OPS_MODE io_pin_mode, LED_STATUS status)
+void led_entity_init (PIN_MAP pin_map, PIN_OPS_MODE pin_io_mode, LED_STATUS status)
 {
     led_entity.pin.pin_map = pin_map;
-    led_entity.pin.io_pin_mode = io_pin_mode;
+    led_entity.pin.pin_io_mode = pin_io_mode;
 
     led_entity.status = status;
 }

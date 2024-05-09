1fcd0e9a99fb2c184000911bde848fbb76d6a04f
oqazett <oqazett@gmail.com>
2024-05-09 20:13:48 +0900

    feat:[GwangSeon Eo] PWM request form을 선언하는 pwm_request.h 추가

---
+++ 5th/gseo/ddd_pwm/domain/pwm/service/request/pwm_request.h
@@ -0,0 +1,16 @@
+#ifndef __PWM_REQUEST_H__
+#define __PWM_REQUEST_H__
+
+#include "stdint.h"
+
+struct _pwm_request
+{
+    volatile unsigned char *pwm_channel_address;
+    uint8_t compare_output_value;
+    uint8_t wave_generation_value;
+    uint8_t pwm_prescale_value;
+};
+
+struct _pwm_request pwm_port_request;
+
+#endif

07796540125708841cb531320beefdeaa60dc65b
oqazett <oqazett@gmail.com>
2024-05-15 15:48:25 +0900

    feat:[GwangSeon Eo] PWM domain repository 테이블을 call 하는 부분을 service에 추가

--- 5th/gseo/ddd_pwm/domain/pwm/service/pwm_service.c
+++ 5th/gseo/ddd_pwm/domain/pwm/service/pwm_service.c
@@ -1,16 +1,19 @@
+#include <stdio.h>
+
 #include "pwm_service.h"
-// #include "../repository/pwm_repository_table.h"
+#include "../repository/pwm_repository_table.h"
+
 
 void pwm_service_compare_output_mode (struct _pwm_request pwm_request)
 {
-    printf("\npwm domain service : compare output mode\n");
-    // pwm_repository_call_table[SET_COMPARE_OUTPUT_MODE](pwm_port_request);    
+    printf("\n[PWM SERVICE] - Compare Output Mode\n");
+    pwm_repository_call_table[SET_COMPARE_OUTPUT_MODE](pwm_request);
 }
 
 void pwm_service_wave_generation_mode (struct _pwm_request pwm_request)
 {
-    printf("\npwm domain service : wave generation mode\n");
-    // pwm_repository_call_table[SET_WAVE_GENERATION_MODE](pwm_port_request);
+    printf("\n[PWM SERVICE] - Wave Generation Mode\n");
+    pwm_repository_call_table[SET_WAVE_GENERATION_MODE](pwm_request);
 }
 
 void pwm_service_prescale (struct _pwm_request pwm_request)

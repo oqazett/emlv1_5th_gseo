5df99d93510bc9d6bc76fc2aafe3c9487725d441
oqazett <oqazett@gmail.com>
2024-06-22 01:04:53 +0900

    feat:[GwangSeon Eo] 필요없는 코드 부분 제거 main.c

--- 5th/gseo/ddd_pwm/main.c
+++ 5th/gseo/ddd_pwm/main.c
@@ -53,53 +53,6 @@ int main(void)
     printf("2. Chipset         : ATmega328p\r\n");
     printf("3. Peripheral      : PWM\r\n");
     printf("4. Software Design : Domain-Driven Design.\r\n\n");
-    //  [DDD-PWM-0]
-    //  TODO : Developer can choose the pin to control.
-    //  [DDD-PWM-7]
-    //  TODO : Developer can choose the pin direction.
-
-#if 0
-    TCCR0A = 0x00; // 노멀모드 초기화
-    TCCR0B = 0x03; //분주비 64
-    TCNT0 = 250;
-    TIMSK0 = 0x01; // overflow 인터럽트 발생가능
-    sei(); // 인터럽트 사용
-#endif
-    // [DDD-PWM-1]
-    // TODO : Developers can set the PWM Mode.
-    // [DDD-PWM-7]
-    // TODO : Timer/Counter0,1,2 Select
-
-// 240619 22:11
-#if 0
-    set_pin_control_form(PIN_PD6, PIN_OUTPUT_MODE);
-    pin_service_call_table[PIN_DIRECTION] (
-        convert_pin_control_data(pin_control_form)
-    );
-
-    // [DDD-PWM-3]
-    // TODO : Developers choose specification of Timer/Counter
-    set_pwm_control_form(PWM_CHANNEL_OC0A, COMPARE_OUTPUT_SET, FAST_PWM_OCRx, PWM_PRESCALE_64);
-    pwm_service_call_table[PWM_COMPARE_OUTPUT_MODE](
-        convert_pwm_control_data(pwm_control_form) );
-    pwm_service_call_table[PWM_WAVE_GENERATION_MODE](
-        convert_pwm_control_data(pwm_control_form) );
-    pwm_service_call_table[PWM_PRESCALE](
-        convert_pwm_control_data(pwm_control_form) );
-
-
-    //  [DDD-PWM-0]
-    //  TODO : Developer can choose the pin to control.
-    //  [DDD-PWM-7]
-    //  TODO : Developer can choose the pin direction.
-    set_pin_control_form(PIN_PB5, PIN_OUTPUT_MODE);
-    pin_service_call_table[PIN_DIRECTION] (
-        convert_pin_control_data(pin_control_form) );
-
-    TIMSK0 = 0x01; // overflow 인터럽트 발생가능
-    OCR0A = 0xff;
-    sei(); // 인터럽트 사용
-#endif   
 
 /* 240621 Code Revision */
 #if 1
@@ -122,9 +75,6 @@ int main(void)
     );
 #endif
 
-    // INIT_TIMER();
-    int i, j = 0;
-
     for(;;)
     {
         // [DDD-PWM-8]
@@ -139,18 +89,6 @@ int main(void)
         //     convert_led_control_data(led_control_form));
         // _delay_ms(500);
 
-        /**************************************/
-        // for(i=PULSE_MIN; i<=PULSE_MAX; i+=20)
-        // {
-        //     OCR1A = i;
-        //     _delay_ms(10);
-        // }
-        // for(j=PULSE_MAX; j>=PULSE_MIN; j-=20)
-        // {
-        //     OCR1A = j;
-        //     _delay_ms(10);
-        // }
-        
         OCR1A = 1000;
         _delay_ms(1000);
         OCR1A = 3000;

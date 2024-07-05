1174339cd49ba25b0ac9abd853045a3de7b17150
oqazett <oqazett@gmail.com>
2024-07-05 17:09:44 +0900

    style:[GwangSeon Eo] Wave generation mode를 Repository에서 처리할 시, switch 문이 아닌 함수 포인터 형식으로 처리하도록 코드 스타일 변경

--- 5th/gseo/ddd_pwm/domain/pwm/repository/pwm_repository.c
+++ 5th/gseo/ddd_pwm/domain/pwm/repository/pwm_repository.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <avr/io.h>
 
+
 void set_compare_output_mode(struct _pwm_request pwm_request){
     switch(pwm_request.compare_output_value){
         case 0:
@@ -21,43 +22,9 @@ void set_compare_output_mode(struct _pwm_request pwm_request){
 }
 
 void set_wave_generation_mode(struct _pwm_request pwm_request){
-    switch(pwm_request.wave_generation_value){
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
-        case 8:
-            break;
-        case 9:
-            break;
-        case 10:
-            break;
-        case 11:
-            break;
-        case 12:
-            break;
-        case 13:
-            break;
-        case 14:
-            TCCR1A |= (1<<WGM11);
-            TCCR1B |= (1<<WGM12) | (1<<WGM13); //고속 PWM 모드, TOP : ICR1
-            break;
-        case 15:
-            break;
-        
-    }
+
+    tc16_wave_generation_mode_table[pwm_request.wave_generation_value]();
+    
 }
 
 void set_prescale(struct _pwm_request pwm_request){
@@ -83,10 +50,84 @@ void set_prescale(struct _pwm_request pwm_request){
 }
 
 void set_pwm_spec_for_sg90_servo_motor(struct _pwm_request pwm_request){
-    
+
     set_compare_output_mode(pwm_request);
     set_wave_generation_mode(pwm_request);
     set_prescale(pwm_request);
 
     ICR1 = 40000; // 20ms period
+}
+
+const TC16_wave_generation_mode_handler tc16_wave_generation_mode_table[] = {
+    TC16_wave_generation_mode_value_0,
+    TC16_wave_generation_mode_value_1,
+    TC16_wave_generation_mode_value_2,
+    TC16_wave_generation_mode_value_3,
+    TC16_wave_generation_mode_value_4,
+    TC16_wave_generation_mode_value_5,
+    TC16_wave_generation_mode_value_6,
+    TC16_wave_generation_mode_value_7,
+    TC16_wave_generation_mode_value_8,
+    TC16_wave_generation_mode_value_9,
+    TC16_wave_generation_mode_value_10,
+    TC16_wave_generation_mode_value_11,
+    TC16_wave_generation_mode_value_12,
+    TC16_wave_generation_mode_value_13,
+    TC16_wave_generation_mode_value_14,
+    TC16_wave_generation_mode_value_15
+
+};
+
+
+void TC16_wave_generation_mode_value_0(void){
+
+}
+void TC16_wave_generation_mode_value_1(void){
+
+}
+void TC16_wave_generation_mode_value_2(void){
+
+}
+void TC16_wave_generation_mode_value_3(void){
+
+}
+void TC16_wave_generation_mode_value_4(void){
+
+}
+void TC16_wave_generation_mode_value_5(void){
+
+}
+void TC16_wave_generation_mode_value_6(void){
+
+}
+void TC16_wave_generation_mode_value_7(void){
+
+}
+void TC16_wave_generation_mode_value_8(void){
+
+}
+void TC16_wave_generation_mode_value_9(void){
+
+}
+void TC16_wave_generation_mode_value_10(void){
+
+}
+void TC16_wave_generation_mode_value_11(void){
+
+}
+void TC16_wave_generation_mode_value_12(void){
+
+}
+void TC16_wave_generation_mode_value_13(void){
+
+}
+void TC16_wave_generation_mode_value_14(void){
+    printf("[PWM REPOSITORY] Mode : Fast PWM, TOP : ICR1 settings...");
+    TCCR1A |= (1<<WGM11);
+    TCCR1B |= (1<<WGM12) | (1<<WGM13); //고속 PWM 모드, TOP : ICR1
+
+    printf(" Done!\n");
+}
+void TC16_wave_generation_mode_value_15(void){
+
 }
--- 5th/gseo/ddd_pwm/domain/pwm/repository/pwm_repository.h
+++ 5th/gseo/ddd_pwm/domain/pwm/repository/pwm_repository.h
@@ -3,6 +3,8 @@
 
 #include "../service/request/pwm_request.h"
 
+#define NUMBER_OF_TC16_WAVE_GENERATION_MODE     16
+
 enum _pwm_domain_repository{
     SET_PWM_SPEC_FOR_SG90_SERVO_MOTOR
 };
@@ -11,7 +13,32 @@ void set_compare_output_mode(struct _pwm_request);
 void set_wave_generation_mode(struct _pwm_request);
 void set_prescale(struct _pwm_request);
 
+typedef void (* TC16_wave_generation_mode_handler) (void);
+
+const TC16_wave_generation_mode_handler \
+      tc16_wave_generation_mode_table[NUMBER_OF_TC16_WAVE_GENERATION_MODE];
+
 void set_pwm_spec_for_sg90_servo_motor (struct _pwm_request);
+
+
+void TC16_wave_generation_mode_value_0(void);
+void TC16_wave_generation_mode_value_1(void);
+void TC16_wave_generation_mode_value_2(void);
+void TC16_wave_generation_mode_value_3(void);
+void TC16_wave_generation_mode_value_4(void);
+void TC16_wave_generation_mode_value_5(void);
+void TC16_wave_generation_mode_value_6(void);
+void TC16_wave_generation_mode_value_7(void);
+void TC16_wave_generation_mode_value_8(void);
+void TC16_wave_generation_mode_value_9(void);
+void TC16_wave_generation_mode_value_10(void);
+void TC16_wave_generation_mode_value_11(void);
+void TC16_wave_generation_mode_value_12(void);
+void TC16_wave_generation_mode_value_13(void);
+void TC16_wave_generation_mode_value_14(void);
+void TC16_wave_generation_mode_value_15(void);
+
+
 
 
 #endif

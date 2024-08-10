6b30376e409f86a9a2e913cc1028d3e7732c35c7
oqazett <oqazett@gmail.com>
2024-08-10 09:17:15 +0900

    chore:[GwangSeon Eo] Compare Output 핀의 A포트와 B포트를 구별하기 위한 output compare pin 인자 추가, 8비트 카운터와 16비트 카운터의 enum prescale 값을 각각 전달 받을 수 있도록 pwm_prescale 변수타입 변경

--- 5th/gseo/ddd_pwm/domain/pwm/form/pwm_control_form.c
+++ 5th/gseo/ddd_pwm/domain/pwm/form/pwm_control_form.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 
 void set_pwm_control_form(PWM_TIMER_COUNTER_NUMBER   pwm_timer_counter_number,
+                          OUTPUT_COMPARE_PIN    output_compare_pin,
                           COMPARE_OUTPUT_MODE   compare_output_mode,
                           WAVE_GENERATION_MODE  wave_generation_mode,
-                          SELECT_PRESCALE_VALUE      pwm_prescale)
+                          uint8_t      pwm_prescale)
 {
     // [DDD-PWM-9]
     // TODO : 50Hz 주기의 PWM 파형에 대한 셋팅 정보를 저장할 control form을 공간을 만든다.
     pwm_control_form.pwm_timer_counter_number = pwm_timer_counter_number;
+    pwm_control_form.output_compare_pin       = output_compare_pin;
     pwm_control_form.compare_output_mode      = compare_output_mode;
     pwm_control_form.wave_generation_mode     = wave_generation_mode;
     pwm_control_form.pwm_prescale             = pwm_prescale;
@@ -21,6 +23,7 @@ struct _pwm_request convert_pwm_control_data(struct _pwm_control_form pwm_contro
     // TODO : control form에 저장되어 있는 16bit fast pwm mode 정보 값을 request form으로 변환한다.
     pwm_request.hw_pwm_address        = pwm_map_address_vo[pwm_control_form.pwm_timer_counter_number];
     pwm_request.timer_counter_channel = pwm_control_form.pwm_timer_counter_number;
+    pwm_request.output_compare_pin    = pwm_control_form.output_compare_pin;
     pwm_request.compare_output_value  = pwm_control_form.compare_output_mode;
     pwm_request.wave_generation_value = pwm_control_form.wave_generation_mode;
     pwm_request.pwm_prescale_value    = pwm_control_form.pwm_prescale;

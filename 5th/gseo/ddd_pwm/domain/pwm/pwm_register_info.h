a40eeeeb201867ab7b80027dd92ed7ef19ca21eb
oqazett <oqazett@gmail.com>
2024-08-10 10:08:33 +0900

    chore:[GwangSeon Eo} Timer/Counter 채널 2에 대한 Wave generation mode 추가

--- 5th/gseo/ddd_pwm/domain/pwm/pwm_register_info.h
+++ 5th/gseo/ddd_pwm/domain/pwm/pwm_register_info.h
@@ -34,34 +34,49 @@ enum _COMPARE_OUTPUT_MODE
     COMPARE_OUTPUT_MODE_END
 };
 
-typedef enum _WAVE_GENERATION_MODE WAVE_GENERATION_MODE;
-enum _WAVE_GENERATION_MODE
+typedef enum _TC1_WAVE_GENERATION_MODE TC1_WAVE_GENERATION_MODE;
+enum TC1_WAVE_GENERATION_MODE
 {
-    NORMAL_TOP_0xFFFF                   =   0,
-    PHASE_CORRECT_8BIT_TOP_0x00FF       =   1,
-    PHASE_CORRECT_9BIT_TOP_0x01FF       =   2,
-    PHASE_CORRECT_10BIT_TOP_0x01FF      =   3,
-    CTC_TOP_OCR1A                       =   4,
-    FAST_PWM_8BIT_TOP_0x00FF            =   5,
-    FAST_PWM_9BIT_TOP_0x01FF            =   6,
-    FAST_PWM_10BIT_TOP_0x03FF           =   7,
-    PHASE_AND_FREQ_CORRECT_TOP_ICR1     =   8,
-    PHASE_AND_FREQ_CORRECT_TOP_OCR1A    =   9,
-    PHASE_CORRECT_TOP_ICR1              =   10,
-    PHASE_CORRECT_TOP_OCR1A             =   11,
-    CTC_TOP_ICR1                        =   12,
-    RESERVED                            =   13,
-    FAST_PWM_TOP_ICR1                   =   14,
-    FAST_PWM_TOP_OCR1A                  =   15,   
+    TC1_NORMAL_TOP_0xFFFF                   =   0,
+    TC1_PHASE_CORRECT_8BIT_TOP_0x00FF       =   1,
+    TC1_PHASE_CORRECT_9BIT_TOP_0x01FF       =   2,
+    TC1_PHASE_CORRECT_10BIT_TOP_0x01FF      =   3,
+    TC1_CTC_TOP_OCR1A                       =   4,
+    TC1_FAST_PWM_8BIT_TOP_0x00FF            =   5,
+    TC1_FAST_PWM_9BIT_TOP_0x01FF            =   6,
+    TC1_FAST_PWM_10BIT_TOP_0x03FF           =   7,
+    TC1_PHASE_AND_FREQ_CORRECT_TOP_ICR1     =   8,
+    TC1_PHASE_AND_FREQ_CORRECT_TOP_OCR1A    =   9,
+    TC1_PHASE_CORRECT_TOP_ICR1              =   10,
+    TC1_PHASE_CORRECT_TOP_OCR1A             =   11,
+    TC1_CTC_TOP_ICR1                        =   12,
+    TC1_RESERVED                            =   13,
+    TC1_FAST_PWM_TOP_ICR1                   =   14,
+    TC1_FAST_PWM_TOP_OCR1A                  =   15,   
 
-    WAVE_GENERATION_MODE_END
+    TC1_WAVE_GENERATION_MODE_END
+};
+
+typedef enum _TC2_WAVE_GENERATION_MODE TC2_WAVE_GENERATION_MODE;
+enum TC2_WAVE_GENERATION_MODE
+{
+    TC2_NORMAL_TOP_0xFF                   =   0,
+    TC2_PHASE_CORRECT_TOP_0xFF            =   1,
+    TC2_CTC_TOP_OCRA                      =   2,
+    TC2_FAST_PWM_TOP_0xFF                 =   3,
+    TC2_RESERVED_0                        =   4,
+    TC2_PHASE_CORRECT_TOP_OCRA            =   5,
+    TC2_RESERVED_1                        =   6,
+    TC2_FAST_PWM_TOP_OCR1A                =   7,   
+
+    TC2_WAVE_GENERATION_MODE_END
 };
 
 typedef enum _TC1_SELECT_PRESCALE_VALUE TC1_SELECT_PRESCALE_VALUE;
 enum TC1_SELECT_PRESCALE_VALUE
-{                                               // *** If Timer/Counter 0 or 2 ***
-    TC1_NO_CLOCK_SOURCE                     =   0,  // 0 : NO_CLOCK_SOURCE      //
-    TC1_PRESCALE_1                          =   1,  //
+{                                               
+    TC1_NO_CLOCK_SOURCE                     =   0,  
+    TC1_PRESCALE_1                          =   1,  
     TC1_PRESCALE_8                          =   2,
     TC1_PRESCALE_64                         =   3,
     TC1_PRESCALE_256                        =   4,

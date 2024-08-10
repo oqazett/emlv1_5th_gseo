cb13d2c916cc41b860e0bbab2413f453914a9215
oqazett <oqazett@gmail.com>
2024-08-10 10:15:27 +0900

    feat:[GwangSeon Eo] DC 모터를 구동하기 위한 PWM 설정 정보를 control form에 전달하고 PWM Service에 설정 요청

--- 5th/gseo/ddd_pwm/main.c
+++ 5th/gseo/ddd_pwm/main.c
@@ -27,6 +27,9 @@
 #define USE_SG90_SERVO_MOTOR    0
 #define USE_DC_MOTOR            1
 
+
+#define DELAY					2000
+
 int main(void)
 {
     //  [DDD-PWM-6]
@@ -45,7 +48,7 @@ int main(void)
     // [DDD-PWM-9]
     // TODO : control form에 50Hz PWM 파형을 생성하기 위한
     //        compare output mode, wave generation mode, precale 정보를 전달한다.
-    set_pwm_control_form(PWM_TIMER_COUNTER_1, NON_INVERTING_MODE, FAST_PWM_TOP_ICR1, PRESCALE_8);
+    set_pwm_control_form(PWM_TIMER_COUNTER_1, NON_INVERTING_MODE, FAST_PWM_TOP_ICR1, TC1_PRESCALE_8);
     
     // [DDD-PWM-8]
     // TODO : 개발자는 SG90 서보모터를 동작시키기 위해
@@ -62,24 +65,81 @@ int main(void)
 
 /* 240724 DC MOTOR */
 #if USE_DC_MOTOR
+//  [DDD-PWM-9]
+//  TODO : control form에 250KHz PWM 파형을 생성하기 위한
+//         compare output mode, wave generation mode, precale 정보를 전달한다.
+    set_pwm_control_form(PWM_TIMER_COUNTER_2, OCxB ,NON_INVERTING_MODE, TC2_FAST_PWM_TOP_0xFF, TC2_PRESCALE_64);
+    
+// [DDD-PWM-9]
+// TODO : 개발자는 DC모터를 동작시키기 위해
+//        PWM를 설정해야 하는데, 이 때 PWM Setting 정보들을 Service에 요청하는 코드를 작성한다.
+    pwm_service_call_table[PWM_SETTING_DC_MOTOR](
+        convert_pwm_control_data(pwm_control_form)
+    );
 
+    set_pin_control_form(PIN_PD3, PIN_OUTPUT_MODE);
+    pin_service_call_table[PIN_DIRECTION] (
+        convert_pin_control_data(pin_control_form)
+    );
+
+    set_pin_control_form(PIN_PD4, PIN_OUTPUT_MODE);
+    pin_service_call_table[PIN_DIRECTION] (
+        convert_pin_control_data(pin_control_form)
+    );
+
+    set_pin_control_form(PIN_PD5, PIN_OUTPUT_MODE);
+    pin_service_call_table[PIN_DIRECTION] (
+        convert_pin_control_data(pin_control_form)
+    );
 #endif
-    for(;;)
-    {
-        if(USE_SG90_SERVO_MOTOR==1){
-            pwm_service_call_table[ROTATE_SG90_MOTOR_90_DEGREE_CW](
-                convert_pwm_control_data(pwm_control_form)
-            );
-            _delay_ms(1000);
+/* Servo Motor Code */
+    // for(;;)
+    // {
+    //     if(USE_SG90_SERVO_MOTOR==1){
+    //         pwm_service_call_table[ROTATE_SG90_MOTOR_90_DEGREE_CW](
+    //             convert_pwm_control_data(pwm_control_form)
+    //         );
+    //         _delay_ms(1000);
 
-            pwm_service_call_table[ROTATE_SG90_MOTOR_90_DEGREE_CCW](
-                convert_pwm_control_data(pwm_control_form)
-            );
-            _delay_ms(1000);
-        }
+    //         pwm_service_call_table[ROTATE_SG90_MOTOR_90_DEGREE_CCW](
+    //             convert_pwm_control_data(pwm_control_form)
+    //         );
+    //         _delay_ms(1000);
+    //     }
 
-        if(USE_DC_MOTOR==1){
-            
-        }
+    //     if(USE_DC_MOTOR==1){
+    //         OCR1A = 400;
+    //         _delay_ms(1000);
+    //     }
+	// }
+
+// ********************************************************************
+
+    while(1)
+	{
+		OCR2B = 0;              // 모터정지  
+		PORTD |= 0b00110000;    // PD4:1 PD5:1  모터 정지
+		_delay_ms(DELAY);       // 2초 
+		
+		PORTD &= 0b11011111;       // PD5 : 0
+		PORTD |= 0b00010000;       // 정방향 회전 PD4:1  PD5: 0   
+		OCR2B = 127;        // 0.5ms 되면 비교 일치 발생 될 수 있도록 한다. 
+		_delay_ms(DELAY);   // 2초 
+		OCR2B = 255;        // 1ms    
+		_delay_ms(DELAY);   // 2초 
+	
+		OCR2B = 0;              // 모터 정지 	
+		PORTD |= 0b00110000;    // PD4:1 PD5:1  모터 정지
+		_delay_ms(DELAY);
+		PORTD &= 0b11101111;
+		PORTD |= 0b00100000;       // 역회전 PD4: 0 PD5: 1
+		OCR2B = 127;
+		_delay_ms(DELAY);
+		OCR2B = 255;
+		_delay_ms(DELAY);
+		 
 	}
+	return 0;
+
+
 }
